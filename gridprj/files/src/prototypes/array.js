01cd25a8b48192f98d5558ed8f880ae488ce4000
nightwolfcem <38102744+nightwolfcem@users.noreply.github.com>
2025-07-25 13:40:36 +0300

    Delete gridprj/files/src directory

--- gridprj/files/src/prototypes/array.js
+++
@@ -1,9 +0,0 @@
-﻿Array.prototype.filterRemove = function(predicate) {
-let rtn = [];
-for (let i = this.length - 1; i >= 0; i--) {
-if (predicate(this[i], i, this)) {
-rtn.push(...this.splice(i, 1));
-}
-}
-return rtn;
-};
