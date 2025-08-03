02979d10c09d54313b994a44af48470155021ba7
nightwolfcem <38102744+nightwolfcem@users.noreply.github.com>
2025-08-03 10:57:42 +0300

    Update TlegacyPropEditor.js

--- gridprj/files/js/src/ui/prop-editor/TlegacyPropEditor.js
+++ gridprj/files/js/src/ui/prop-editor/TlegacyPropEditor.js
@@ -655,5 +655,5 @@ this.status.sizable = true;
 
   }
 }
-DOM.loadFuncs.push(_=>DOM.addStyleSheet(globs.path + 'files/css/propeditor.css'));
+DOM.loadFuncs.push(_=>DOM.addStyleSheet(DOM.getUpPath(null, 2) + "files/css/propeditor.css'));
 
