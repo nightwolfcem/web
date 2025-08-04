8a6420b67edbc70eadad0fde75b97540a4b389d2
nightwolfcem <38102744+nightwolfcem@users.noreply.github.com>
2025-08-04 21:39:44 +0300

    Update legacy-prop-editor-demo.js

--- gridprj/grild/legacy-prop-editor-demo.js
+++ gridprj/grild/legacy-prop-editor-demo.js
@@ -1,10 +1,12 @@
 import '../files/js/src/main.js';
 import { TpropEditor as TlegacyPropEditor } from '../files/js/src/ui/prop-editor/TlegacyPropEditor.js';
-
+import {allClass} from '../files/js/src/core/classUtils.js';
 document.addEventListener('DOMContentLoaded', () => {
-  const app = document.getElementById('app');
-  const editor = new TlegacyPropEditor(600, 400);
+export  const app = document.getElementById('app');
+export  const editor = new TlegacyPropEditor(600, 400,{closeMode:"hide");
+export allClass=allClass;
   editor.body(app);
   editor.viewObject(document.body);
   editor.show();
+
 });
